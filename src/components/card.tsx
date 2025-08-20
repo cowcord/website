54254aef550537b22581ab598f04f248e3f0c0e7
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-08-20 02:32:34 -0700

    add start of download page
    gonna finish ts then squash

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -24,3 +24,19 @@ export const Card = component$((props: CardProps) => {
 		</Wrapper>
 	);
 });
+
+// todo: make work
+export const CardDropdown = component$((props: CardProps) => {
+	return (
+		<div
+			class={{
+				'bg-muted m-5 flex h-auto w-auto max-w-80 flex-col gap-y-3 rounded-lg px-5 py-3': true,
+				'hover:bg-muted-variant/60': !!props.href,
+			}}
+		>
+			{props.emoji ? <h1 class="-mb-1 text-4xl">{props.emoji}</h1> : null}
+			<h1 class="text-muted-foreground text-xl">{props.title}</h1>
+			{props.description ? <p class="text-muted-foreground/70">{props.description}</p> : null}
+		</div>
+	);
+});
--- src/routes/download/index.tsx
+++ src/routes/download/index.tsx
@@ -1,4 +1,7 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useVisibleTask$ } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+import { Card } from '@comp/card';
+import { getComputerInfo } from '@lib/utils/computer';
 
 export interface DownloadPageParams {
 	os?: string;
@@ -6,5 +9,34 @@ export interface DownloadPageParams {
 }
 
 export default component$(() => {
-	return <div></div>;
+	let [os, arch] = getComputerInfo();
+
+	useVisibleTask$(() => {
+		let params = new URLSearchParams(window.location.search);
+		arch.value = params.get('arch') ?? arch.value;
+		os.value = params.get('os') ?? os.value;
+	});
+
+	const tags = getReleaseTags().value;
+
+	return (
+		<div class="flex flex-col">
+			<h1 class="text-bold mt-[10rem] ml-[10rem] text-4xl text-white" data-for-arch="x64">
+				Download cowcord{os.value ? ` for ${os.value}` : ''}
+			</h1>
+			<Card
+				title={`Download${os.value ? ` for ${os.value}${arch.value ? ` ${os.value === 'MacOS' ? 'Silicon' : arch.value}` : ''}` : ''}`}
+				href=""
+			/>
+		</div>
+	);
+});
+
+// download file format:
+// https://github.com/cowcord/cowcord/releases/download/<tag name>/<file name>
+
+const getReleaseTags = routeLoader$(async () => {
+  const resp = await fetch('https://api.github.com/repos/cowcord/cowcord/tags');
+  const data = await resp.json();
+  return data.map((tag: { name: string }) => tag.name);
 });
